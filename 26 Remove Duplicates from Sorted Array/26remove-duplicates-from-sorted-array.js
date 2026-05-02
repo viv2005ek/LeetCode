308f4b51280d7a16a8c8d77a9257a6f50af52a67
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-02 11:18:34 +0000

    Code challenge submission for question: 26 Remove Duplicates from Sorted Array

---
+++ 26 Remove Duplicates from Sorted Array/26remove-duplicates-from-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+
+    let k = 1; // position for next unique
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
