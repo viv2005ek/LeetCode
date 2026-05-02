d10daf7dd7542273ad4c5d0429af54340d8ef843
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-02 21:43:37 +0000

    Code challenge submission for question: 80 Remove Duplicates from Sorted Array II

---
+++ 80 Remove Duplicates from Sorted Array II/80remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+        if (nums.length <= 2) return nums.length;
+
+    let k = 2; // first two always valid
+
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 2]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
