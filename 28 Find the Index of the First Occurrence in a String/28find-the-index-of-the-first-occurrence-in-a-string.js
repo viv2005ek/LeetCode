7d7af92f4633f1042121b381926d4da525ac0727
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-02 11:24:39 +0000

    Code challenge submission for question: 28 Find the Index of the First Occurrence in a String

---
+++ 28 Find the Index of the First Occurrence in a String/28find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let n = haystack.length;
+    let m = needle.length;
+
+    for (let i = 0; i <= n - m; i++) {
+        let j = 0;
+
+        while (j < m && haystack[i + j] === needle[j]) {
+            j++;
+        }
+
+        if (j === m) return i;
+    }
+
+    return -1;
+};
