628a8f8303b24b729e5a3be527b797c202820e89
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-03 01:03:53 +0000

    Code challenge submission for question: 107 Binary Tree Level Order Traversal II

---
+++ 107 Binary Tree Level Order Traversal II/107binary-tree-level-order-traversal-ii.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    if (!root) return [];
+
+    let result = [];
+    let queue = [root];
+
+    while (queue.length) {
+        let size = queue.length;
+        let level = [];
+
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        result.push(level);
+    }
+
+    return result.reverse();
+};
