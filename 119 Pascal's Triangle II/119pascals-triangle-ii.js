8111ce92a9c995d47a28bed0920f2ab1ea09c579
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-04 21:05:41 +0000

    Code challenge submission for question: 119 Pascal's Triangle II

---
+++ 119 Pascal's Triangle II/119pascals-triangle-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    let row = new Array(rowIndex + 1).fill(0);
+    row[0] = 1;
+
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i; j >= 1; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+    }
+
+    return row;
+};
