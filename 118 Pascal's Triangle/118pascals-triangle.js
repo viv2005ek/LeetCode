15f1e85c8a34c59ababd551cd34ba6737dea9f9c
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-04 21:05:01 +0000

    Code challenge submission for question: 118 Pascal's Triangle

---
+++ 118 Pascal's Triangle/118pascals-triangle.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let result = [];
+
+    for (let i = 0; i < numRows; i++) {
+        let row = new Array(i + 1).fill(1);
+
+        for (let j = 1; j < i; j++) {
+            row[j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+
+        result.push(row);
+    }
+
+    return result;
+};
