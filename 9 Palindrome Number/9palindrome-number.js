9cda632a07ba6a6ff4aee2b76d693a2de1ddb68b
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-04 17:12:42 +0000

    Code challenge submission for question: 9 Palindrome Number

---
+++ 9 Palindrome Number/9palindrome-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    // negatives or trailing zero (except 0)
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reversed = 0;
+
+    while (x > reversed) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+   
+    return x === reversed || x === Math.floor(reversed / 10);
+};
