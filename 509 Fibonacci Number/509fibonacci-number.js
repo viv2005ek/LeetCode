a5b1db0acd13bc7203c256fd0d2bffd96f83d00f
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-06 21:47:38 +0000

    Code challenge submission for question: 509 Fibonacci Number

---
+++ 509 Fibonacci Number/509fibonacci-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if (n <= 1) return n;
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+};
