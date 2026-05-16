a82da57790aeae4925e977cc32e6f88e29685753
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-16 01:01:50 +0000

    Code challenge submission for question: 154 Find Minimum in Rotated Sorted Array II

---
+++ 154 Find Minimum in Rotated Sorted Array II/154find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        // Minimum is in right half
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        }
+        // Minimum is in left half including mid
+        else if (nums[mid] < nums[right]) {
+            right = mid;
+        }
+        // Duplicate case
+        else {
+            right--;
+        }
+    }
+
+    return nums[left];
+};
