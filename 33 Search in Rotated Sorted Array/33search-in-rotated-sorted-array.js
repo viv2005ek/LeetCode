f7d1d2074099d4cf41ca631aa7610f758c8ee2a7
Vivek Kumar Garg <viv2005ek@gmail.com>
2026-05-22 00:25:50 +0000

    Code challenge submission for question: 33 Search in Rotated Sorted Array

---
+++ 33 Search in Rotated Sorted Array/33search-in-rotated-sorted-array.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        // Left half is sorted
+        if (nums[left] <= nums[mid]) {
+
+            // Target lies inside left sorted half
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+        } 
+        // Right half is sorted
+        else {
+
+            // Target lies inside right sorted half
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+};
