2d57f292dcac2e55f0664d20b2b6d1ae9c7481fe
Vivek Kumar Garg <142710112+viv2005ek@users.noreply.github.com>
2026-08-13 23:21:24 +0530

    Time: 28 ms (79.83%), Space: 61.2 MB (25.19%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let map = new Map();
+    for(let char of s){
+        map.set(char,(map.get(char)||0)+1);
+    }
+   for(let i =0;i<s.length;i++){
+    if(map.get(s[i])==1) return i;
+   }
+   return -1;
+};
